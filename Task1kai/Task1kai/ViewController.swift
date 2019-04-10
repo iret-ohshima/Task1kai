5ecfb50855ce6f38e8af87d244a6a86917e9fc55
大島 佑太 <ohshima@cloudpack.jp>
2019-04-10 19:06:56 +0900

    Xcodeプロジェクト作成

---
+++ Task1kai/Task1kai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task1kai
+//
+//  Created by 大島 佑太 on 2019/04/10.
+//  Copyright © 2019 大島 佑太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
